2e83039156857e6e3a43dcb08d9337d189a129e2
dawka <udawaadulam9611@gmail.com>
2022-09-09 19:20:53 +0800

    dd

---
+++ CVBs/basic_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from basic_app import views
+
+app_name = 'basic_app'
+
+urlpatterns = [
+    path('',views.SchoolListView.as_view(),name='list'),
+    path('(?P<pk>\d+)/',views.SchoolDetailView.as_view(),name='detail'),
+    path('create/',views.SchoolCreateView.as_view(),name='create'),
+    path('update/(?P<pk>\d+)/',views.SchoolUpdateView.as_view(),name='update'),
+    path('delete/(?P<pk>\d+)/',views.SchoolDeleteView.as_view(),name='delete')
+]
